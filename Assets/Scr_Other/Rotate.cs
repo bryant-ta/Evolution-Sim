65cb873ef85d3a2299298c73a1d69ebcc437b2eb
bryant-ta <33585269+bryant-ta@users.noreply.github.com>
2019-07-31 14:59:28 -0700

    Initial
    Project structure and folders

---
+++ Assets/Scr_Other/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 15;
+
+    private void Update()
+    {
+        transform.Rotate(transform.forward, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scr_Other/Stats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+
+
+    void Awake()
+    {
+
+    }
+}
