ec70b7e8a306875e1c9cf6204881630e9fa31e96
bryant-ta <33585269+bryant-ta@users.noreply.github.com>
2019-08-01 02:42:32 -0700

    MoveForward
    Creatures can move forward

---
+++ Assets/Scr_Traits/MoveForward.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MoveForward : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (rb.velocity.magnitude < speed)
+        {
+            rb.AddForce(transform.right * speed * 100);
+        }
+    }
+}
